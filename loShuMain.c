78721e475dd187c74ee0f46992bf7f6b2b397d5f
blongite <ethanphamxd@gmail.com>
2026-03-20 15:11:28 -0700

    add dupeCheck function, modified main

--- loShuMain.c
+++ loShuMain.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-//#include <stdlib.h>
+#include <time.h>
+#include <stdlib.h>
 
 int magic (int arr[3][3], int rows, int columns) {
     // Row T sum initialization
@@ -10,7 +11,7 @@ int magic (int arr[3][3], int rows, int columns) {
         sum += arr[0][i]; // Hard-coded because I know the length will always be 3.
         
     }
-    printf("row T ok \n");
+    //printf("row T ok \n");
     // Row M check
     for (int i = 0; i < columns; i++) {
         checkSum += arr[1][i];
@@ -21,7 +22,7 @@ int magic (int arr[3][3], int rows, int columns) {
     if (checkSum != sum) {
         return -1; // -1 will indicate a false value for the magic function.
     }
-    printf("row M ok \n");
+    //printf("row M ok \n");
 
     // Row B check
     checkSum = 0;
@@ -32,7 +33,7 @@ int magic (int arr[3][3], int rows, int columns) {
     if (checkSum != sum) {
         return -1; 
     }
-    printf("row B ok \n");
+    //printf("row B ok \n");
     // Column L check
     checkSum = 0;
     for (int i = 0; i < rows; i++) {
@@ -42,7 +43,7 @@ int magic (int arr[3][3], int rows, int columns) {
     if (checkSum != sum) {
         return -1; 
     }
-    printf("col L ok \n");
+    //printf("col L ok \n");
     // Column M check
     checkSum = 0;
     for (int i = 0; i < rows; i++) {
@@ -52,7 +53,7 @@ int magic (int arr[3][3], int rows, int columns) {
     if (checkSum != sum) {
         return -1; 
     }
-    printf("col M ok \n");
+    //printf("col M ok \n");
     // Column R check
     checkSum = 0;
     for (int i = 0; i < rows; i++) {
@@ -62,7 +63,7 @@ int magic (int arr[3][3], int rows, int columns) {
     if (checkSum != sum) {
         return -1; 
     }
-    printf("col R ok \n");
+    //printf("col R ok \n");
     // Diagonal L to R check
     checkSum = 0;
     for (int i = 0; i < columns; i++) {
@@ -73,7 +74,7 @@ int magic (int arr[3][3], int rows, int columns) {
     if (checkSum != sum) {
         return -1; 
     }
-    printf("diag L to R ok \n");
+    //printf("diag L to R ok \n");
     // Diagonal R to L check
     checkSum = 0;
     //printf("%d", checkSum);
@@ -85,28 +86,127 @@ int magic (int arr[3][3], int rows, int columns) {
     if (checkSum != sum) {
         return -1; 
     }
-    printf("diag R to L ok \n");
+    //printf("diag R to L ok \n");
 
     return 0;
+}
+
+
+int dupeCheck (int arr[3][3], int value) {
+
+    for (int i = 0; i < 3; i++) {
+
+            for (int j = 0; j < 3; j++) {
+                
+                if (value == arr[i][j]) {
+                    //printf("%d = %d.\n", value, arr[i][j]);
+                    return -1; // if there's a dupe
+                }
+
+                //printf("%d != %d!\n", value, arr[i][j]);
+            }
+            //printf("\n");
+            
+    }
+
+    return 0; // If there's no dupe
 }
 
 int main (void) {
     // Initialize square 2D array
     int square[3][3];
-    int fart;
+    int otherSquare[3][3];
+    int s;
+    int os;
+    int attempts = 0;
+
+    srand(time(0));
+    const int MAX = 9;
+    const int MIN = 1;
 
     square[0][0] = 4, square[0][1] = 9, square[0][2] = 2,
     square[1][0] = 3, square[1][1] = 5, square[1][2] = 7,
     square[2][0] = 8, square[2][1] = 1, square[2][2] = 6,
 
     
-    fart = magic(square, 3, 3);
+    s = magic(square, 3, 3);
 
-    if (fart == 0) {
+    if (s == 0) {
         printf("This square is a magic square!!!!1!1!11\n");
     } else {
         printf("This square is NOT a magic square !!1\n");
     }
+
+    while (os != 0) {
+        attempts++;
+
+        for (int i = 0; i < 3; i++) {
+
+            for (int j = 0; j < 3; j++) {
+                // generate a random number
+                int r = rand() % (MAX - MIN + 1) + MIN;
+
+                // check if r already appears in the array
+
+                if (dupeCheck(otherSquare, r) == 0) {
+                    otherSquare[i][j] = r;
+                    //printf("Passed Dupe Test: %d\n", otherSquare[i][j]);
+                    
+                } else {
+                    while (dupeCheck(otherSquare, r) != 0) {
+                        r = rand() % (MAX - MIN + 1) + MIN;
+                        //printf("%d", r);
+                    }
+
+                    otherSquare[i][j] = r;
+                    //printf("Passed Dupe Test, Post-Dupe Hell: %d\n", otherSquare[i][j]);
+                }
+                //printf("%d\n", otherSquare[i][j]);
+            }
+            
+        }
+        /*
+        printf("Loop Over! Results:\n");
+
+        for (int i = 0; i < 3; i++) {
+
+            for (int j = 0; j < 3; j++) {
+                
+                printf("%d ", otherSquare[i][j]);
+                
+            }
+             printf("\n");
+            
+        }
+        */
+        os = magic(otherSquare, 3, 3);
+        
+        // clear array if it is not a magic square
+        if (os != 0) {
+            for (int m = 0; m < 3; m++) {
+                for (int n = 0; n < 3; n++) {
+                    otherSquare[m][n] = 0;
+
+                }
+            }
+        }
+        
+        
+    }
+    printf("RANDOM MAGICAL SQUARE:\n");
+    printf("Attempts: %d\n", attempts);
+        
+    for (int i = 0; i < 3; i++) {
+
+            for (int j = 0; j < 3; j++) {
+                
+                printf("%d ", otherSquare[i][j]);
+                
+            }
+             printf("\n");
+            
+        }
+
 
     return 0;
 }
