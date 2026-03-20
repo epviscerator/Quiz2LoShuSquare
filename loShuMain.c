9281f52c31787249cd862ea2e129182e6aebd2e0
blongite <ethanphamxd@gmail.com>
2026-03-20 11:34:52 -0700

    Initial Commit

---
+++ loShuMain.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+
+int main (void) {
+    // Initialize square 2D array
+    int square[3][3]
+
+    magic(square, 3, 3)
+
+    return 0;
+}
+
+int magic (int arr[3][3], int rows, int columns) {
+    // Row T check
+    
+    // Row M check
+
+    // Row B check
+
+    // Column L check
+
+    // Column M check
+
+    // Column R check
+
+    // Diagonal L to R check
+
+    // Diagonal R to L check
+
+    return 0;
+}
